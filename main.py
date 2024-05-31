7bad2f5eecc3910c46d8a08e197495a68c5a84b6
moefaq <124541328+moefaq@users.noreply.github.com>
2024-06-01 03:12:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,86 @@
+import aiohttp.web
+import yaml
+import aiohttp
+import logging
+import re
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
+    level=logging.INFO,
+)
+
+
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
+
+headers = {"User-Agent": "clash"}
+
+
+async def buildSubData(yamlData):
+    try:
+        with open("template.yml", "r") as f:
+            templateData = yaml.load(f, yaml.CUnsafeLoader)
+
+        subYamlData = templateData
+
+        proxies = yamlData["proxies"]
+        subYamlData["proxies"] = proxies
+
+        proxiesNames = []
+        for proxy in proxies:
+            proxiesNames.append(proxy["name"])
+
+        proxyGroups = templateData["proxy-groups"]
+        for proxyGroup in proxyGroups:
+            if proxyGroup["name"] == "🔰手动选择":
+                proxyGroup["proxies"] = proxiesNames
+            if (
+                proxyGroup["proxies"][0][:2] == "/^"
+                and proxyGroup["proxies"][0][-1] == "/"
+            ):
+                pattern = re.compile(proxyGroup["proxies"][0].replace("/", ""))
+                proxyGroup["proxies"] = []
+                for proxiesName in proxiesNames:
+                    if re.search(pattern, proxiesName):
+                        proxyGroup["proxies"].append(proxiesName)
+
+        subYamlData["proxy-groups"] = proxyGroups
+        return subYamlData
+    except Exception as err:
+        logging.error(err)
+        return
+
+
+async def handle_request(request):
+    apptoken = request.query.get("apptoken")
+    url = request.query.get("url")
+
+    if apptoken not in config["server"]["token"]:
+        return aiohttp.web.Response(text="Invalid apptoken", status=401)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers, allow_redirects=True) as response:
+            respHeaders = {
+                "subscription-userinfo": response.headers["subscription-userinfo"],
+                "profile-update-interval": response.headers["profile-update-interval"],
+                "content-disposition": response.headers["content-disposition"],
+                "profile-web-page-url": response.headers["profile-web-page-url"],
+            }
+            respYamlData = yaml.full_load(await response.text(encoding="utf-8"))
+            yamlData = await buildSubData(respYamlData)
+            if yamlData is not None:
+                return aiohttp.web.Response(
+                    text=yaml.safe_dump(
+                        data=yamlData,
+                        allow_unicode=False,
+                        encoding="utf-8",
+                    ).decode("unicode-escape")
+                )
+            else:
+                return aiohttp.web.Response(text="Nothing!",headers=respHeaders)
+
+
+app = aiohttp.web.Application()
+app.router.add_get("/", handle_request)
+
+aiohttp.web.run_app(app, host=f"{config['server']['listen']}", port=config['server']['port'])
